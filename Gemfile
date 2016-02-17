bea8309371accb03556d527ffa2ff641a7f083ac
Julian Bouma <bouma@post.harvard.edu>
2016-02-17 12:07:44 -0800

    reorganize folders

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'pg'
+
+gem 'uglifier'
+gem 'thin'
+
+gem 'jquery-rails'
+gem 'haml-rails'
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'byebug'
+  gem 'binding_of_caller'
+  gem 'better_errors'
+  gem 'rspec-rails'
+end
+
---
+++ config/application.rb
@@ -0,0 +1,12 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module Shoppe
+  class Application < Rails::Application
+    config.generators.helper = false
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
