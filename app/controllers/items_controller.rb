1648faf15a9f2e315c7b0b81886bc253ebd5344b
Danny Rosenfeld & Umar Farooq Bajwa <pair+Dannyarosenfeld.UmarFBajwa@devbootcamp.com>
2016-06-13 16:47:22 -0700

    Add link to create new form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
   end
 
   def create
+    @item = Item.new(params[:item])
+
+    @item.save
+    redirect_to @item
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :description)
+  end
+
 end
