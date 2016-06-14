208004af6906a8fc5e814b5db47722b9dd9b6e86
Danny Rosenfeld & Umar Farooq Bajwa <pair+Dannyarosenfeld.UmarFBajwa@devbootcamp.com>
2016-06-14 12:42:50 -0700

    Add updates on feature tests for CRUD

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -8,31 +8,39 @@ RSpec.feature "Items", type: :feature, js: true do
     expect(page).to have_content(item.name)
   end
 
-    it "creates a new item" do
-      visit new_item_path
-      fill_in 'Name', with: item.name
-      fill_in 'Price', with: item.price
-      fill_in 'Description', with: item.description
-      click_on('Submit')
-      expect(page).to have_content(item.description)
-    end
+  it "creates a new item" do
+    visit new_item_path
+    fill_in 'Name', with: item.name
+    fill_in 'Price', with: item.price
+    fill_in 'Description', with: item.description
+    click_on('Submit')
+    expect(page).to have_content(item.name.upcase)
 
-    before(:each) do
-      visit root_path
-    end
+  end
 
-    it "displays a form and updates an existing post" do
-      new_price = 20
-      click_on('Edit')
-      visit edit_item_path(item.id)
-      fill_in('Name', with: new_price)
-      click_on('Submit')
-      expect(page).to have_content(new_price)
-    end
-    it "has a button that deletes the item" do
-      click_on('Delete')
-      page.driver.browser.switch_to.alert.accept
-      expect(page).to_not have_content(item.name)
-    end
+  before(:each) do
+    visit root_path
+  end
+
+  it "displays a specific item" do
+    click_on('Show')
+    expect(page).to have_content(item.name.upcase)
+    # expect(page).to have_content(item.price)
+    # expect(page).to have_content(item.description)
+  end
+
+  it "displays a form and updates an existing item" do
+    new_price = 20
+    click_on('Edit')
+    visit edit_item_path(item.id)
+    fill_in('Name', with: new_price)
+    click_on('Submit')
+    expect(page).to have_content(new_price)
+  end
+  it "has a button that deletes the item" do
+    click_on('Delete')
+    page.driver.browser.switch_to.alert.accept
+    expect(page).to_not have_content(item.name)
+  end
 
 end
