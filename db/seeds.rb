2d1cded3b28915034904a90aeed553b023404f4b
Umar Farooq Bajwa <email": null>
2016-06-15 10:08:01 -0700

    Add Bootstrap and styling to New Page and Form Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+require 'faker'
+
+ Item.delete_all
+ 10.times do
+     Item.create( name: Faker::Commerce.item_name,
+     description: Faker::Company.catch_phrase,
+     image_url: 'http://lorempixel.com/400/200/',
+     quantity: (1..30).to_a.sample,
+     price: Faker::Commerce.price )
+ end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
