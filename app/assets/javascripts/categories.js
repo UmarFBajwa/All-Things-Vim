ba35d900e0b39ce65a84eb75341aab4003f5039f
Robin Soubry & Raymond Sapida <pair+RobinSoubry.raysapida@devbootcamp.com>
2016-06-17 15:22:03 -0700

    Add ajax listener to add items form

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,2 +1,31 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  addItemListener();
+})
+
+// Add to basket
+function addItemListener(){
+  $('.addItem').on('submit', function(event){
+    event.preventDefault();
+    var data = $(this).serialize();
+    var url = $(this).attr('action');
+    var context = this;
+
+    var ajaxRequest = $.ajax({
+      data: data,
+      url: url,
+      context: this,
+      method: 'post'
+    })
+
+    ajaxRequest.done(function(msg) {
+      // alert(this);
+      console.log(msg);
+      $('.cart').append(msg);
+
+    });
+
+    ajaxRequest.fail(function(msg){
+      console.log("Fail");
+    });
+  });
+}
