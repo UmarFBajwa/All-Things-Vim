f4b4d761c1b10397046bca56a01e963e748e49ee
Robin Soubry & Raymond Sapida <pair+RobinSoubry.raysapida@devbootcamp.com>
2016-06-17 15:17:59 -0700

    Add ordered_items as resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, except: :index
   resources :items
   resources :categories
+  resources :ordered_items, only: [:create]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
