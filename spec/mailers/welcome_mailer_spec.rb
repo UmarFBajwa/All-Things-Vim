457917009d3964d5b50598e93c457ee17e52822a
Raymond Sapida <ray.sapida@gmail.com>
2016-06-18 08:49:04 -0700

    Add spesc for the WelcomeMailer class

--- spec/mailers/welcome_mailer_spec.rb
+++ spec/mailers/welcome_mailer_spec.rb
@@ -1,5 +1,26 @@
 require "rails_helper"
 
 RSpec.describe WelcomeMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create :user }
+  let(:mail) { described_class.welcome_email(user).deliver_now }
+
+  it 'renders the subject' do
+    expect(mail.subject).to eq('Welcome to the Vim Shop')
+  end
+
+  it 'renders the receiver email' do
+    expect(mail.to).to eq([user.email])
+  end
+
+  it 'renders the sender email' do
+    expect(mail.from).to eq(['francis.vimmer@gmail.com'])
+  end
+
+  it 'assigns @name' do
+    expect(mail.body.encoded).to match(user.name)
+  end
+
+  it 'sends an email' do
+    expect{ mail }.to change(ActionMailer::Base.deliveries, :size).by(1)
+  end
 end
