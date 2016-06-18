5e39fa9a17ccaca7dcb818d57c8ef64ab8e35328
Raymond Sapida <ray.sapida@gmail.com>
2016-06-18 09:09:21 -0700

    Add specs for SendEmailJob

--- spec/jobs/send_email_job_spec.rb
+++ spec/jobs/send_email_job_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe SendEmailJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  let(:user) { create :user }
+
+  it 'queues the job' do
+    expect {
+      described_class.perform_later(user)
+    }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'is the default queue' do
+    expect(SendEmailJob.new.queue_name).to eq 'default'
+  end
+
+  it 'executes perform' do
+    job = double('job')
+    allow(WelcomeMailer).to receive(:welcome_email).with(user).and_return(job)
+    allow(job).to receive(:deliver_later)
+
+    expect(WelcomeMailer).to receive(:welcome_email).with(user)
+
+    perform_enqueued_jobs { described_class.perform_later(user)}
+  end
 end
