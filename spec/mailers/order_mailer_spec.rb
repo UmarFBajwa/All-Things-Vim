a645f540a041de9842ff8299858d2a587f7b331c
Raymond Sapida <ray.sapida@gmail.com>
2016-06-18 18:52:46 -0700

    Add specs for OrderMailer

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -1,5 +1,23 @@
 require "rails_helper"
 
 RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create :user }
+  let!(:order) { create(:order, user: user) }
+  let(:mail) { described_class.order_confirmation(user).deliver_now }
+
+  it 'renders the subject' do
+    expect(mail.subject).to eq('Your order confirmation')
+  end
+
+  it 'renders the receiver email' do
+    expect(mail.to).to eq([user.email])
+  end
+
+  it 'renders the sender email' do
+    expect(mail.from).to eq(['francis.vimmer@gmail.com'])
+  end
+
+  it 'sends an email' do
+    expect{ mail }.to change(ActionMailer::Base.deliveries, :size).by(1)
+  end
 end
